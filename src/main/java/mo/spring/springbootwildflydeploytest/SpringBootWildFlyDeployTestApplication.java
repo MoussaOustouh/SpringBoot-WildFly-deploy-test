532dfc20e5070cd8f11c6c22fb760f361e4e0c59
Moussa OUSTOUH <moussa.oustouh@gmail.com>
2022-06-29 23:48:19 +0100

    init

---
+++ src/main/java/mo/spring/springbootwildflydeploytest/SpringBootWildFlyDeployTestApplication.java
@@ -0,0 +1,13 @@
+package mo.spring.springbootwildflydeploytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWildFlyDeployTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWildFlyDeployTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mo/spring/springbootwildflydeploytest/SpringBootWildFlyDeployTestApplicationTests.java
@@ -0,0 +1,13 @@
+package mo.spring.springbootwildflydeploytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWildFlyDeployTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
