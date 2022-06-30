5a6d16b0791ca164fea65838bd5f02efb1ea2eb7
Moussa OUSTOUH <moussa.oustouh@gmail.com>
2022-06-30 02:01:52 +0100

    extends SpringBootServletInitializer and override SpringApplicationBuilder configure(SpringApplicationBuilder builder)

--- src/main/java/mo/spring/springbootwildflydeploytest/SpringBootWildFlyDeployTestApplication.java
+++ src/main/java/mo/spring/springbootwildflydeploytest/SpringBootWildFlyDeployTestApplication.java
@@ -2,12 +2,19 @@ package mo.spring.springbootwildflydeploytest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootWildFlyDeployTestApplication {
+public class SpringBootWildFlyDeployTestApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootWildFlyDeployTestApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(SpringBootWildFlyDeployTestApplication.class);
     }
 
 }
